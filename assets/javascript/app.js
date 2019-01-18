c2f83b3fb5c46bee27fc2b577bd9081eea6716cc
thedanielenrico <thedanielenrico@gmail.com>
2019-01-17 17:59:58 -0800

    Start making question array

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// create start scenario with button
+// start timer and show questions that takes user input
+// when user submits an answer, show them if they were correct or not
+// move onto next question
+
+var questionArray = [{question: 'What color is the sky', answer: 'Blue', posibility: 'Red ', posibility:'Orange', posibility:'Green'}, {question: 'What color is George Washingtons white horse', answer: 'White', posibility: 'Red', posibility: 'Orange', posibility: 'Green'}];
+
+
+console.log(questionArray[0].answer);
