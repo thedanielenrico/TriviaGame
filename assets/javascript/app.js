43831299cd04560917c8eb7e85ea9d06fae81964
thedanielenrico <thedanielenrico@gmail.com>
2019-01-20 09:09:58 -0800

    Built out timer functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,14 @@
 var correct = 0;
 var incorrect = 0;
 var $app = $("#app");
+var $timer = $("#timerBox");
 var questionIndex = 0;
-var timer = 15;
-var remainingTime;
+var timer;
 var isRunning = false;
+var intervalId;
 
+
+// Question object array
 var questions = [
     {
         q: 'What color is the sky? ',
@@ -19,6 +22,7 @@ var questions = [
     }
 ]
 
+// Start function
 function init() {
     var $start = $("<button class='btn btn-secondary'>Start</button>");
     $app.empty();
@@ -30,58 +34,87 @@ function init() {
 }
 init();
 
-
+// Show question when button is pressed
 function showQuestion() {
     $app.empty();
-    remainingTime = 15;
     var question = questions[questionIndex];
     var $question = $("<div>");
-    // var $clock = $("<div>Time Remaining: <span id='clock'>" + remainingTime + "</span></div>")
     var $q = $("<h2>" + question.q + "</h2>");
     var $button;
-    // timer = setInterval(decrement, 1000);
-    // isRunning = true;
-    // $question.append($clock);
-    $question.append($q);
     for (var i = 0; i < question.choices.length; i++) {
         $button = $("<button>" + question.choices[i] + "</button>")
         $button.on("click", handleAnswerButton)
         $question.append($button);
     }
-    $question.append($q);
+    $question.prepend($q);
     $app.append($question);
     runTimer();
+    timer = 15;
 }
+
+// Answer button handler
 function handleAnswerButton() {
-    var value = $(this).text()
-    var answer = questions[0].value;
-    if (value === answer) {
-        console.log("You win")
-        correct++;
-    } else {
-        console.log("nah")
-        incorrect++;
-    }
-    questionIndex++;
-    showQuestion();
+    var value = $(this).text();
+    displayAnswer(value);
 }
+// Function for displaying answer
+function displayAnswer(userInput) {
+    $app.empty();
+    var question = questions[questionIndex];
+    $app.append("<h2>Answer</h2>");
+    $app.append("<h3>Correct answer is: " + question.answer + "</h3>");
+    $app.append("<h3>You selected: " + userInput + "</h3>");
+    console.log(question.answer);
+    if (userInput === undefined) {
+        $app.append("<h3>You ran out of time!</h3>");
+        incorrect++;
+        timeOut;
+        stopTimer();
+    } else if (userInput === question.answer) {
+        $app.append("<h3>You win!</h3>");
+        correct++;
+        stopTimer();
+    } else {
+        $app.append("<h3>You lose!</h3>");
+        incorrect++;
+        stopTimer();
+    }
+
+
+    questionIndex++;
+    if (questionIndex < questions.length) {
+        setTimeout(showQuestion, 5000);
+    } else {
+        setTimeout(endGame, 5000);
+    }
+}
+
+// Timer functions
 function stopTimer() {
     isRunning = false;
-    clearInterval(timer);
+    clearInterval(intervalId);
+    $timer.empty();
 };
 function runTimer() {
     if (!isRunning) {
-        timer = setInterval(decrement, 1000);
+        intervalId = setInterval(decrement, 1000);
         isRunning = true;
     }
 };
 function decrement() {
     var $clock = $("<div>Time Remaining: <span id='clock'>" + timer + "</span></div>");
-    $app.html($clock);
     timer--;
+    $timer.html($clock);
     if (timer === 0) {
         stopTimer();
     }
+}
+
+function endGame(){
+    $app.empty();
+    $app.append("<h2>Your correct answer total is: " + correct + "</h2>");
+    $app.append("<h2>Your incorrect answer total is: " +incorrect+"</h2>");
+    
 }
 
 
